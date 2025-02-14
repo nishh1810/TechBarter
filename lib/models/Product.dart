4fbc7d1e46e3d0dc122149d2192cb5221c06cfd4
Nishita <101450921@georgebrown.ca>
2025-02-14 11:35:53 -0500

    product updated

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:tech_barter/models/RefType.dart';
 
 class Product {
@@ -8,8 +6,13 @@ class Product {
   String? description;
   double? price;
   int? quantity;
+  bool? active;
+  bool? isRecyclable;
+  int? totalReview;
+  double? avgRating;
   RefType? category;
   RefType? seller;
+  List<RefType>? images;
 
   Product(
       {this.id,
@@ -17,18 +20,30 @@ class Product {
         this.description,
         this.price,
         this.quantity,
+        this.isRecyclable,
+        this.active,
+        this.totalReview,
+        this.avgRating,
         this.category,
-        this.seller});
+        this.seller,
+        this.images
+      });
 
   Product.fromJson(Map<String, dynamic> json) {
+    // print("Product.fromJson: $json");
     try {
       id = json['id'];
       name = json['name'];
       description = json['description'];
       price = json['price'];
       quantity = json['quantity'];
+      isRecyclable = json['recyclable'];
+      active = json['active'];
+      totalReview = json['totalReview'];
+      avgRating = json['avgRating'];
       category = json['category'] != null ? RefType.fromJson(json['category']) : null;
       seller = json['seller'] != null ? RefType.fromJson(json['seller']) : null;
+      images = json["images"] != null ? List<RefType>.from(json["images"]!.map((x) => RefType.fromJson(x))) : null;
     } catch (e) {
       print("Error in Product.fromJson: $e");
     }
@@ -42,17 +57,24 @@ class Product {
     data['description'] = description;
     data['price'] = price;
     data['quantity'] = quantity;
+    data['recyclable'] = isRecyclable;
+    data['active'] = active;
+    data['totalReview'] = totalReview;
+    data['avgRating'] = avgRating;
     if (category != null) {
       data['category'] = category!.toJson();
     }
     if (seller != null) {
       data['seller'] = seller!.toJson();
     }
+    if (images != null) {
+      data['images'] = images!.map((v) => v.toJson()).toList();
+    }
     return data;
   }
 
   @override
   String toString() {
-    return 'Product(id: $id, name: $name, price: $price, quantity: $quantity)';
+    return 'Product(id: $id, name: $name, price: $price, quantity: $quantity, isRecyclable: $isRecyclable, active: $active, totalReview: $totalReview, avgRating: $avgRating, category: $category, seller: $seller, images: $images)';
   }
 }
