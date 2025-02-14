53e459687542a25ab6281795c71205006b5cde3c
Nishita <101450921@georgebrown.ca>
2025-02-14 11:36:25 -0500

    reftype updated

--- lib/models/RefType.dart
+++ lib/models/RefType.dart
@@ -1,5 +1,3 @@
-import 'package:tech_barter/models/object_id.dart';
-
 class RefType {
   String? id;
   String? sRef;
@@ -14,9 +12,15 @@ class RefType {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     if (id != null) {
-      data['_id'] = id;
+      data['id'] = id;
     }
     data['_ref'] = sRef;
     return data;
   }
+
+  @override
+  String toString() {
+    return 'RefType(id: $id, sRef: $sRef)';
+  }
+
 }
