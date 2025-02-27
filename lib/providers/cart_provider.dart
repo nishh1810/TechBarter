39bd67b24a3c548aa0024ca11c2ca5b7274162ec
Nishita <101450921@georgebrown.ca>
2025-02-27 11:39:59 -0500

    cart provider updated

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:tech_barter/models/Items.dart';
-import 'package:tech_barter/models/Product.dart';
 import 'package:tech_barter/models/cart_product.dart';
 import 'package:tech_barter/providers/auth_provider.dart';
 import 'package:tech_barter/services/api_service.dart';
@@ -26,12 +25,24 @@ class CartProvider extends ChangeNotifier {
     return _totalAmount;
   }
 
+  double _taxAmount = 0.0;
+  double get taxAmount {
+    _taxAmount = _totalAmount * 0.13;
+    return _taxAmount;
+  }
+
+  double _finalAmount = 0.0;
+  double get finalAmount {
+    _finalAmount = totalAmount + taxAmount;
+    return _finalAmount;
+  }
+
   CartProvider() {
     getCartItems();
   }
 
   Future<void> addToCart(String productId, String userId, int quantity) async {
-    try {
+    // try {
       final url = '$apiUrl/cart';
 
       final response = await http.post(
@@ -51,11 +62,12 @@ class CartProvider extends ChangeNotifier {
         await getCartItems();
         notifyListeners();
       }else {
+        print("response : ${response.body.toString()}");
         throw Exception("Failed to add to cart");
       }
-    }catch(e) {
-      print(e.toString());
-    }
+    // }catch(e) {
+    //   print(e.toString());
+    // }
   }
 
   Future<void> getCartItems() async {
@@ -82,4 +94,49 @@ class CartProvider extends ChangeNotifier {
       print(e.toString());
     }
   }
+
+  Future<void> removeFromCart(String productId) async {
+    try {
+      final url = '$apiUrl/cart/removeProduct/$productId';
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': AuthProvider.token!,
+        },
+      );
+      if(response.statusCode == 200) {
+        await getCartItems();
+        notifyListeners();
+      } else {
+        throw Exception("Failed to remove product from cart");
+      }
+    }catch(e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> clearCart() async {
+    try {
+      final url = '$apiUrl/cart/remove';
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': AuthProvider.token!,
+        },
+      );
+      if(response.statusCode == 200) {
+        cartItems.clear();
+        print("cart clear response : ${response.body.toString()}");
+        notifyListeners();
+      } else {
+        throw Exception("Failed to load cart items");
+      }
+    }catch(e) {
+      print(e.toString());
+    }
+  }
+
+
 }
