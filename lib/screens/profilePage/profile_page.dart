5fd55adbcb658e2c5bc489e70de10c3685f0e3f2
Hardik <h66000977@gmail.com>
2025-03-10 11:56:47 -0400

    profile page created

---
+++ lib/screens/profilePage/profile_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:tech_barter/components/custom_scaffold.dart';
+
+import 'components/my_profile.dart';
+import 'components/profile_side_menu.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 64, vertical: 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: ProfileSideMenu( index: 0,),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: MyProfile(),
+                  ),
+                ]
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
